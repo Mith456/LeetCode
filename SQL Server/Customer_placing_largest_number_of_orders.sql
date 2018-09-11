5edcbe201b6f32348c4c6a67dcaaf605d02bda5b
Mithun Muralidhar <mithunmurali456@gmail.com>
2018-09-10 22:34:06 -0700

    Create Customer_placing_largest_number_of_orders.sql

---
+++ SQL Server/Customer_placing_largest_number_of_orders.sql
@@ -0,0 +1,38 @@
+Query the customer_number from the orders table for the customer who has placed the largest number of orders.
+It is guaranteed that exactly one customer will have placed more orders than any other customer.
+
+The orders table is defined as follows:
+
+| Column            | Type      |
+|-------------------|-----------|
+| order_number (PK) | int       |
+| customer_number   | int       |
+| order_date        | date      |
+| required_date     | date      |
+| shipped_date      | date      |
+| status            | char(15)  |
+| comment           | char(200) |
+
+Sample Input
+
+| order_number | customer_number | order_date | required_date | shipped_date | status | comment |
+|--------------|-----------------|------------|---------------|--------------|--------|---------|
+| 1            | 1               | 2017-04-09 | 2017-04-13    | 2017-04-12   | Closed |         |
+| 2            | 2               | 2017-04-15 | 2017-04-20    | 2017-04-18   | Closed |         |
+| 3            | 3               | 2017-04-16 | 2017-04-25    | 2017-04-20   | Closed |         |
+| 4            | 3               | 2017-04-18 | 2017-04-28    | 2017-04-25   | Closed |         |
+
+Sample Output
+
+| customer_number |
+|-----------------|
+| 3               |
+
+# MY SOLUTION
+
+SELECT Customer_number, count(*) as total_orders
+from orders
+group by 1
+order by 2 desc
+limit 1
+
