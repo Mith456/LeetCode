33aa97df51711fda0bda3e7d5785004bdf8f160d
Mithun Muralidhar <mithunmurali456@gmail.com>
2018-09-12 21:27:38 -0700

    CREATE Department_Highest_Salary.sql

---
+++ SQL Server/Department_Highest_Salary.sql
@@ -0,0 +1,37 @@
+The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, Max has the highest salary in the IT department and Henry has the highest salary in the Sales department.
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| Sales      | Henry    | 80000  |
++------------+----------+--------+
+
+
+MY SOLUTION: USED WINDOWS FUNCTION 
+
+SELECT Department, Employee, salary
+FROM(
+SELECT d.name as Department, e.name as Employee, salary, dense_rank() OVER (PARTITION BY d.Id ORDER BY Salary DESC)AS rank_col
+FROM employee e
+JOIN department d
+on e.departmentid=d.id) sub
+WHERE sub.rank_col =1
