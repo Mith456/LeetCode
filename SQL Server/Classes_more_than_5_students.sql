f59b0e7c7a6f5801932bcdedccad835567215681
Mithun Muralidhar <mithunmurali456@gmail.com>
2018-09-14 21:03:58 -0700

    CREATE Sql_coding

---
+++ SQL Server/Classes_more_than_5_students.sql
@@ -0,0 +1,26 @@
+There is a table courses with columns: student and class
+
+Please list out all classes which have more than or equal to 5 students.
+
+For example, the table:
+
++---------+------------+
+| student | class      |
++---------+------------+
+| A       | Math       |
+| B       | English    |
+| C       | Math       |
+| D       | Biology    |
+| E       | Math       |
+| F       | Computer   |
+| G       | Math       |
+| H       | Math       |
+| I       | Math       |
++---------+------------+
+
+# MY SOLUTION:
+ 
+SELECT Class
+FROM Courses
+GROUP BY Class
+HAVING COUNT(DISTINCT student) >=5
